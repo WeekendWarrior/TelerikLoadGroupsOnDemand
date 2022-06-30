b49e958563ec336007e560f69c5b62ca4f892872
Steven Striga <sstriga@gmail.com>
2022-06-30 11:11:33 -0400

    Updating repo logic

--- TelerikLoadGroupsOnDemand/Shared/ServiceResponse.cs
+++ TelerikLoadGroupsOnDemand/Shared/ServiceResponse.cs
@@ -6,8 +6,6 @@ public class ServiceResponse<T>
 {
     public bool Success { get; set; }
 
-    public string? Message { get; set; }
-
     public T? Data { get; set; }
 
     public List<AggregateFunctionsGroup> GroupedData { get; set; } = new();
